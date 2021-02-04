0ed3c955b40cf03a48f3ea3de9597f7e50c04f5d
Reymark Torres <reymark.torres08.dev@gmail.com>
2021-02-04 15:00:42 +0800

    useRef & useEffect: DOM interaction > clean up

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -17,31 +17,8 @@ function Tilt({children}) {
       'max-glare': 0.5,
     })
 
-    return function cleanup() {
-      tiltNode.vanillaTilt.destroy()
-    }
+    return () => tiltNode.vanillaTilt.destroy()
   }, [])
-
-  // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
-  // div look fancy.
-  // 💰 like this:
-  // const tiltNode = tiltRef.current
-  // VanillaTilt.init(tiltNode, {
-  //   max: 25,
-  //   speed: 400,
-  //   glare: true,
-  //   'max-glare': 0.5,
-  // })
-  //
-  // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
-  // object to your DOM node to cleanup:
-  // `return () => tiltNode.vanillaTilt.destroy()`
-  //
-  // 💰 Don't forget to specify your effect's dependencies array! In our case
-  // we know that the tilt node will never change, so make it `[]`. Ask me about
-  // this for a more in depth explanation.
-
-  // 🐨 add the `ref` prop to the `tilt-root` div here:
   return (
     <div ref={tiltRef} className="tilt-root">
       <div className="tilt-child">{children}</div>
